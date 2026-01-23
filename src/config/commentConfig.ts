a696da02af593b046c87fdae73de55395e3fce09
Aloha <4537aloha-hub@users.noreply.github.com>
2026-01-23 22:18:54 +0800

    修补无法评论问题

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置
 	twikoo: {
-		envId: "https://www.naruseshiroha.top/",
+		envId: "https://twikoo-mauve-seven-52.vercel.app/",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
